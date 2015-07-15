c73c5772cabba07eea2cf76ed380a81df6cd1798
Dean Sofer <ProLoser@users.noreply.github.com>
2015-07-15 13:46:51 -0700

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,13 @@
  *   config: {object}
  * @return {Function}         init function to access shared resources
  */
+ 
+// Executed one time when bot loads
 module.exports = function init(options){
+  // Executed for each channel plugin is activated on
   return function plugin(channel){
+    
+    // Return a hash of regex patterns to listen for
     return {
       // ProLoser types in "ping"
       "ping": function(from, matches) {
